694bca64e1a6c7b519cbe4fd04f6b2368da25bda
Erkan Keskin <keskin.erkan@gmail.com>
2019-09-30 00:49:25 +0300

    Create laravel project and create model -a

---
+++ backend/database/factories/OfficeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\Office;
+use Faker\Generator as Faker;
+
+$factory->define(Office::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
