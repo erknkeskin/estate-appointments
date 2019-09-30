7be532baeb3eedee6bc048d3be59722216233d36
erknkeskin <keskin.erkan@gmail.com>
2019-10-01 01:36:43 +0300

    create office resource and routes

--- backend/app/Http/Controllers/OfficeController.php
+++ backend/app/Http/Controllers/OfficeController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Office\OfficeResource;
 use App\Model\Office;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class OfficeController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,7 @@ class OfficeController extends Controller
      */
     public function index()
     {
-        //
+        return Office::all();
     }
 
     /**
@@ -30,29 +38,39 @@ class OfficeController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $office = new Office;
+        $office->title = $request->title;
+        $office->address = $request->address;
+        $office->latitude = $request->latitude;
+        $office->longitude = $request->longitude;
+        $office->status = $request->status;
+        $office->save();
+
+        return response()->json([
+            'data' => new OfficeResource($office)
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Office  $office
+     * @param \App\Model\Office $office
      * @return \Illuminate\Http\Response
      */
     public function show(Office $office)
     {
-        //
+        return new OfficeResource($office);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Office  $office
+     * @param \App\Model\Office $office
      * @return \Illuminate\Http\Response
      */
     public function edit(Office $office)
@@ -63,23 +81,28 @@ class OfficeController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Office  $office
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Model\Office $office
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Office $office)
     {
-        //
+        $office->update($request->all());
+
+        return response([
+            'data' => new HomeResource($office)
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Office  $office
+     * @param \App\Model\Office $office
      * @return \Illuminate\Http\Response
      */
     public function destroy(Office $office)
     {
-        //
+        $office->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
---
+++ backend/app/Http/Resources/Office/OfficeResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Office;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OfficeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'address' => $this->address,
+            'latitude' => $this->latitude,
+            'longitude' => $this->longitude,
+            'status' => $this->status
+        ];
+    }
+}
