baabbf1cfebfcb49658c1f35dc290af49124768f
erknkeskin <keskin.erkan@gmail.com>
2019-10-01 02:44:30 +0300

    create customers endpoint and CRUD

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\Customer\CustomerResource;
 use App\Model\Customer;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CustomerController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +22,7 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        return Customer::all();
     }
 
     /**
@@ -30,29 +38,38 @@ class CustomerController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $customer = new Customer;
+        $customer->fullname = $request->fullname;
+        $customer->email = $request->email;
+        $customer->phone = $request->phone;
+        $customer->address = $request->address;
+        $customer->save();
+
+        return response()->json([
+            'data' => new CustomerResource($customer)
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Model\Customer  $customer
+     * @param \App\Model\Customer $customer
      * @return \Illuminate\Http\Response
      */
     public function show(Customer $customer)
     {
-        //
+        return new CustomerResource($customer);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Model\Customer  $customer
+     * @param \App\Model\Customer $customer
      * @return \Illuminate\Http\Response
      */
     public function edit(Customer $customer)
@@ -63,23 +80,27 @@ class CustomerController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Model\Customer  $customer
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Model\Customer $customer
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
+        return response()->json([
+            'data' => new CustomerResource($customer)
+        ], Response::HTTP_CREATED);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Model\Customer  $customer
+     * @param \App\Model\Customer $customer
      * @return \Illuminate\Http\Response
      */
     public function destroy(Customer $customer)
     {
-        //
+        $customer->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }
---
+++ backend/app/Http/Resources/Customer/CustomerResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Customer;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'fullname' => $this->fullname,
+            'email' => $this->email,
+            'phone' => $this->phone,
+            'address' => $this->address,
+            'status' => $this->status
+        ];
+    }
+}
